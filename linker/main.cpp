d90a9f737c98a2514f4d0c8e1666f9d7723e1434
Mars Saxman <mars@synopsys.com>
2016-01-11 15:24:02 -0800

    simple little tokenizer

--- linker/main.cpp
+++ linker/main.cpp
@@ -50,27 +50,104 @@
 //  ESP = EBP
 
 namespace {
-class linker
+class DFG
 {
-	assembler dest;
+	struct {
+		std::string path;
+		size_t line = 0;
+		size_t column = 0;
+		bool valid = true;
+	} parse;
+	std::ostream &fail();
+	void read_op(std::string instruction);
+	void read_term(char prefix, std::string body);
+	void read_token(std::string token);
+	void read_line(std::string text);
 public:
+	void read(std::istream &src);
 	void load(std::string path);
+	bool valid() const { return parse.valid; }
 };
 }
 
-void linker::load(std::string path)
+std::ostream &DFG::fail()
 {
-	// syntax
-	// a file is composed of lines separated by '\n'
-	// if present, a ';' char divides instructions (left) from comment (right)
-	// instructions are tokens separated by whitespace
-	// a token is a sequence of isgraph() chars
-	// whitespace is a sequence of isspace() chars other than '\n'
-	// within a file, lines are processed from top to bottom
-	// within a line, tokens are processed from right to left
-	// tokens perform operations on a notional stack machine
-	// tokens which begin with an ispunct() char represent values or directives 
-	// other tokens represent the names of commands to perform
+	parse.valid = false;
+	std::cerr << parse.path << ":" << std::dec;
+	std::cerr << parse.line << ":" << parse.column << ": ";
+	return std::cerr;
+}
+
+void DFG::read_op(std::string op)
+{
+	fail() << "undefined operation \"" << op << "\"" << std::endl;
+}
+
+void DFG::read_term(char prefix, std::string body)
+{
+	switch (prefix) {
+	default:
+		fail() << "undefined prefix \'" << prefix << "\'" << std::endl;
+	}
+}
+
+void DFG::read_token(std::string token)
+{
+	std::cout << token << " ";
+}
+
+void DFG::read_line(std::string line)
+{
+	// Truncate the line at the comment, if present.
+	size_t end = line.size();
+	for (size_t i = 0; i < end; ++i) {
+		if (';' == line[i]) {
+			end = i;
+		}
+	}
+	while (end > 0) {
+		// Find the length of the token character sequence, if present.
+		size_t start = end;
+		do {
+			char c = line[start - 1];
+			if (isblank(c) || !isgraph(c)) break;
+			--start;
+		} while (start > 0);
+		if (start < end) {
+			// If we found a token, go process it.
+			char c = line[start];
+			if (ispunct(c)) {
+				read_term(c, line.substr(start + 1, end - start - 1));
+			} else {
+				read_op(line.substr(start, end - start));
+			}
+			end = start;
+		} else {
+			// If what we found was not a token, skip it.
+			--end;
+		}
+	}
+}
+
+void DFG::read(std::istream &src)
+{
+	// A serialized DFG is a text file composed of lines containing tokens.
+	// A line consists of a sequence of zero or more tokens followed by an
+	// optional comment, delimited by ';', which continues to end of line.
+	// Tokens are sequences of isgraph() delimited by sequences of isspace().
+	// Lines are evaluated top to bottom, tokens are evaluated right to left.
+	// We will extract the tokens from this stream and eval() them in order.
+	parse.line = 1;
+	for (std::string line; std::getline(src, line); ++parse.line) {
+		read_line(line);
+	}
+}
+
+void DFG::load(std::string path)
+{
+	parse.path = path;
+	std::ifstream file(path);
+	read(file);
 }
 
 int main(int argc, const char *argv[])
@@ -81,10 +158,13 @@ int main(int argc, const char *argv[])
 		std::cerr << "radian-link: fail: no input files" << std::endl;
 		return EXIT_FAILURE;
 	}
-	linker dest;
+	std::cerr << std::showbase;
+	DFG dfg;
+	bool valid = true;
 	for (int i = 1; i < argc; ++i) {
-		dest.load(argv[i]);
+		dfg.load(argv[i]);
+		valid &= dfg.valid();
 	}
-	return EXIT_SUCCESS;
+	return valid? EXIT_SUCCESS: EXIT_FAILURE;
 }
 
