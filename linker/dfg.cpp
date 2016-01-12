82e377e8fce3aed033f3a77d88554e61ca84fa07
Mars Saxman <mars@redecho.org>
2016-01-12 09:36:08 -0800

    deserializing operation nodes

--- linker/dfg.cpp
+++ linker/dfg.cpp
@@ -15,10 +15,42 @@
 
 #include "dfg.h"
 
+const dfg::node *dfg::reader::top()
+{
+	if (stack.empty()) {
+		fail("stack underflow");
+		add(new dummy);
+	}
+	return stack.back();
+}
+
+const dfg::node *dfg::reader::pop()
+{
+	auto out = top();
+	stack.pop_back();
+	return out;
+}
+
+void dfg::reader::push(const dfg::node *n)
+{
+	stack.push_back(n);
+}
+
+dfg::node *const dfg::reader::make(dfg::node *const n)
+{
+	body.emplace_back(std::unique_ptr<node>(n));
+	return n;
+}
+
+void dfg::reader::add(dfg::node *const n)
+{
+	push(make(n));
+}
+
 void dfg::reader::fail(std::string msg)
 {
 	using namespace std;
-	err << dec << pos.line << ":" << pos.column << ": " << msg << endl;
+	err << dec << loc.line << ":" << loc.column << ": " << msg << endl;
 }
 
 void dfg::reader::literal(std::string text)
@@ -40,8 +72,40 @@ void dfg::reader::literal(std::string text)
 			break;
 		}
 	}
-	current.nodes.emplace_back(new dfg::literal(value));
-	stack = current.nodes.size();
+	add(new dfg::literal(value));
+}
+
+void dfg::reader::symbol(std::string name)
+{
+	auto iter = symbols.find(name);
+	const dfg::node *out = nullptr;
+	if (iter != symbols.end()) {
+		out = iter->second;
+	} else {
+		fail("undefined symbol \"" + name + "\"");
+		out = make(new dummy);
+		symbols[name] = out;
+	}
+	push(out);
+}
+
+void dfg::reader::unop(node::type id)
+{
+	add(new dfg::unop(id, pop()));
+}
+
+void dfg::reader::binop(node::type id)
+{
+	add(new dfg::binop(id, pop(), pop()));
+}
+
+void dfg::reader::ternop(node::type id)
+{
+	add(new dfg::ternop(id, pop(), pop(), pop()));
+}
+
+void dfg::reader::compound(node::type id)
+{
 }
 
 void dfg::reader::token(std::string text)
@@ -54,34 +118,35 @@ void dfg::reader::token(std::string text)
 		std::string body = text.substr(1);
 		switch (c) {
 			case '$': literal(body); break;
+			case '%': symbol(body); break;
 			default: fail("unknown token prefix '" + std::string(1, c) + "'");
 		}
 	} else {
+		if (text == "add") {
+			binop(node::add_);
+		}
 		fail("undefined operation \"" + text + "\"");
 	}
 }
 
-void dfg::reader::line(std::string line)
+void dfg::reader::instruction(std::string text)
 {
-	// Truncate the line at the comment, if present.
-	size_t end = line.size();
-	for (size_t i = 0; i < end; ++i) {
-		if (';' == line[i]) {
-			end = i;
-		}
-	}
+	// The body of an instruction consists of operands proceeded by a mnemonic.
+	// Process the tokens right to left, evaluating them with respect to the
+	// stack.
+	size_t end = text.size();
 	while (end > 0) {
 		// Find the length of the token character sequence, if present.
 		size_t start = end;
 		do {
-			char c = line[start - 1];
+			char c = text[start - 1];
 			if (isblank(c) || !isgraph(c)) break;
 			--start;
 		} while (start > 0);
 		if (start < end) {
 			// If we found a token, go process it.
-			pos.column = start;
-			token(line.substr(start, end - start));
+			loc.column = start;
+			token(text.substr(start, end - start));
 			end = start;
 		} else {
 			// If what we found was not a token, skip it.
@@ -90,9 +155,62 @@ void dfg::reader::line(std::string line)
 	}
 }
 
+void dfg::reader::line(std::string text)
+{
+	// If the line begins with a token ending with a colon, the token is a
+	// symbol definition against the instruction result.
+	size_t start = 0;
+	while (start < text.size() && isblank(text[start])) {
+		++start;
+	}
+	std::string defsym;
+	size_t pos = text.find_first_of(':', start);
+	if (pos != std::string::npos) {
+		defsym = text.substr(start, pos - start);
+		start = pos + 1;
+	}
+	// If the line ends with text following a semicolon, that is a comment,
+	// which we will ignore.
+	size_t end = text.size();
+	for (size_t i = start; i < end; ++i) {
+		if (';' == text[i]) {
+			end = i;
+		}
+	}
+	instruction(text.substr(start, end - start));
+	if (!defsym.empty()) {
+		if (symbols.find(defsym) != symbols.end()) {
+			fail("redefined symbol \"" + defsym + "\"");
+			return;
+		}
+		symbols[defsym] = top();
+	}
+}
+
 dfg::reader::reader(dfg &_dest, std::istream &src, std::ostream &_err):
+	err(_err),
 	dest(_dest),
-	err(_err)
+	instructions({
+		{"not", [=](){unop(node::not_);}},
+		{"add", [=](){binop(node::add_);}},
+		{"sub", [=](){binop(node::sub_);}},
+		{"mul", [=](){binop(node::mul_);}},
+		{"div", [=](){binop(node::div_);}},
+		{"quo", [=](){binop(node::quo_);}},
+		{"rem", [=](){binop(node::rem_);}},
+		{"shl", [=](){binop(node::shl_);}},
+		{"shr", [=](){binop(node::shr_);}},
+		{"and", [=](){binop(node::and_);}},
+		{"orl", [=](){binop(node::orl_);}},
+		{"xor", [=](){binop(node::xor_);}},
+		{"ceq", [=](){binop(node::ceq_);}},
+		{"cne", [=](){binop(node::cne_);}},
+		{"clt", [=](){binop(node::clt_);}},
+		{"cgt", [=](){binop(node::cgt_);}},
+		{"cle", [=](){binop(node::cle_);}},
+		{"cge", [=](){binop(node::cge_);}},
+		{"sel", [=](){ternop(node::sel_);}},
+	})
 {
 	// A serialized DFG is a text file composed of lines containing tokens.
 	// A line consists of a sequence of zero or more tokens followed by an
@@ -100,8 +218,8 @@ dfg::reader::reader(dfg &_dest, std::istream &src, std::ostream &_err):
 	// Tokens are sequences of isgraph() delimited by sequences of isspace().
 	// Lines are evaluated top to bottom, tokens are evaluated right to left.
 	// We will extract the tokens from this stream and eval() them in order.
-	pos.line = 1;
-	for (std::string text; std::getline(src, text); ++pos.line) {
+	loc.line = 1;
+	for (std::string text; std::getline(src, text); ++loc.line) {
 		line(text);
 	}
 }
--- linker/dfg.h
+++ linker/dfg.h
@@ -28,35 +28,88 @@ public:
 	class node
 	{
 	public:
+		enum type {
+			nil_ = 0,
+			// atoms
+			lit_,
+			// unary
+			not_,
+			// binary
+			add_, sub_, mul_, div_, quo_, rem_,
+			shl_, shr_, and_, orl_, xor_,
+			ceq_, cne_, clt_, cgt_, cle_, cge_,
+			// ternary
+			sel_,
+		};
+		type id() const { return t; }
 		virtual ~node() {}
+	protected:
+		node(type _t): t(_t) {}
+	private:
+		type t;
 	};
-	class literal : public node
+	class dummy: public node
+	{
+	public:
+		dummy(): node(nil_) {}
+	};
+	class literal: public node
 	{
 		int64_t value;
 	public:
-		literal(int64_t v): value(v) {}
+		literal(int64_t v): node(lit_), value(v) {}
 	};
-	class block
+	class unop: public node
 	{
+		const node *v;
 	public:
-		std::vector<std::unique_ptr<node>> nodes;
+		unop(type _t, const node *_v): node(_t), v(_v) {}
+	};
+	class binop: public node
+	{
+		const node *l;
+		const node *r;
+	public:
+		binop(type _t, const node *_l, const node *_r):
+			node(_t), l(_l), r(_r) {}
+	};
+	class ternop: public node
+	{
+		const node *w;
+		const node *t;
+		const node *f;
+	public:
+		ternop(type _i, const node *_w, const node *_t, const node *_f):
+			node(_i), w(_w), t(_t), f(_f) {}
 	};
 	void read(std::istream &src, std::ostream &log);
 private:
-	std::map<std::string, std::unique_ptr<block>> blocks;
 	class reader
 	{
 		struct {
 			size_t line = 1;
 			size_t column = 0;
-		} pos;
+		} loc;
 		std::ostream &err;
 		dfg &dest;
-		block current;
-		size_t stack = 0;
+		std::vector<std::unique_ptr<node>> body;
+		std::vector<const node*> stack;
+		std::map<std::string, const node*> symbols;
+		std::map<std::string, std::function<void()>> instructions;
+		const node *top();
+		const node *pop();
+		void push(const node*);
+		node*const make(node*const);
+		void add(node*const);
 		void fail(std::string message);
 		void literal(std::string body);
+		void symbol(std::string name);
 		void token(std::string token);
+		void instruction(std::string text);
+		void unop(node::type id);
+		void binop(node::type id);
+		void ternop(node::type id);
+		void compound(node::type id);
 		void line(std::string text);
 	public:
 		reader(dfg &dest, std::istream &src, std::ostream &err);
