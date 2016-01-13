06d27e203afeea6c7df6d8c6ec206845d1e0c956
Mars Saxman <mars@synopsys.com>
2016-01-13 11:47:35 -0800

    scrap that approach and try this simpler one

--- linker/main.cpp
+++ linker/main.cpp
@@ -16,45 +16,7 @@
 #include <iostream>
 #include <fstream>
 #include "assembler.h"
-#include "reader.h"
 #include "logstream.h"
-
-// NAME MANGLING
-// one char for each atom, delimiter pairs for compound types:
-//  b = byte, s = short, w = word, l = long
-//  f = float, d = double
-// a structure simply concatenates its field types
-// a nested structure is wrapped in '{' and '}'
-// an array wraps its element type in '[' and ']'
-// a closure wraps its parameter type in '(' and ')' 
-
-// MEMORY LAYOUT
-// structure fields use natural alignment in order
-// arrays are a struct describing a range of elements:
-// template<typename T> struct array<T>{ T *head; T *tail; };
-// a closure combines a proc ptr and an environment value:
-//	struct { void *env; void(*proc)(param); }
-
-// ABI
-// each block invocation gets a parameter and an environment value
-// parameters are provided in registers, according to type:
-//  byte: AL
-//  index: EAX
-//  fixed: EAX, ECX [low, high]
-//  float: ST0
-//  array: EAX:ECX [head, tail]
-//  closure: EAX:ECX [env, proc]
-//  structure: EAX (passed by address)
-// environment is passed by address in EBP
-// the values of other registers are undefined
-
-// MEMORY
-// allocate memory by decrementing ESP using some even multiple of 4
-// deallocate before jumping to the next block, depending on parameter type:
-// closure, struct, array:
-//	ESP = min(EBP, EAX & ~3)
-// any other type:
-//  ESP = EBP
 
 int main(int argc, const char *argv[])
 {
@@ -64,12 +26,11 @@ int main(int argc, const char *argv[])
 	if (argc <= 1) {
 		log << "no input files" << std::endl;
 	}
-	dfg::unit data;
 	for (int i = 1; i < argc; ++i) {
 		std::string path(argv[i]);
 		std::ifstream src(path);
 		logstream err(path + ":", log);
-		dfg::reader(data, src, err);
+		// do something
 	}
 	return log.empty()? EXIT_SUCCESS: EXIT_FAILURE;
 }
