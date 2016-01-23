5bb6e2f0e453d387800cce662d5b38e1a263e023
Mars Saxman <mars@redecho.org>
2016-01-22 22:38:18 -0800

    another round of operator tweaks, exploring continuation-passing style

--- linker/operators.h
+++ linker/operators.h
@@ -1,3 +1,19 @@
+// Copyright 2016 Mars Saxman.
+//
+// Radian is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 2 of the License, or (at your option) any later
+// version.
+//
+// Radian is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// Radian. If not, see <http://www.gnu.org/licenses/>.
+
+
 // Macro system for generating instruction boilerplate.
 // Define macros UNARY, BINARY, TERNARY, and VARIADIC before including this
 // file, then do something useful with the names provided. This header will
@@ -20,41 +36,46 @@
 #define VARIADIC(x) OPERATOR(x)
 #endif
 
-UNARY(jump)		// block: execute a fully bound closure and return its result
-UNARY(notl)		// bool -> bool:logical inversion
-				// int -> int: bitwise inversion
-UNARY(test)		// int -> bool: true if not zero, false if equal to zero
-UNARY(null)		// array -> bool: true if array contains no elements
+// boolean logic
+UNARY(test)		// 0 if 0, otherwise 1
+UNARY(notl)		// 1 if 0, otherwise 0
+BINARY(xorl)	// 0 if both values are zero or nonzero, otherwise 1
+VARIADIC(orl)	// 1 if any operand is nonzero, otherwise 0
+VARIADIC(andl)	// 1 if all operands are nonzero, otherwise 0
+
+// basic integer arithmetic
+UNARY(notw)		// invert all bits
+VARIADIC(orw)	// bitwise or all operand values together
+VARIADIC(andw)	// bitwise and all operand values together
+BINARY(xorw)	// exclusive or of all bits
+VARIADIC(addw)	// add all values together
+BINARY(subw)	// difference between the two values
+
+// numeric comparisons
+VARIADIC(cpeq)	// true if all values are the same
+VARIADIC(cpge)	// true if each value is greater than or equal to the previous
+VARIADIC(cpgt)	// true if each value is greater than the previous one
+
+// functions and control flow
+BINARY(jump)	// closure, value: invoke closure with argument value
+BINARY(bind)	// closure, value: partial application, creates closure
+TERNARY(sel)	// bool, tval, fval: return tval if true, fval if false
+
+// data structures
+VARIADIC(tuple)	// value* -> tuple: fixed group of heterogeneous values
+VARIADIC(array) // value* -> array: variable-length group of like values
+VARIADIC(join)	// struct* -> struct: join elements into a larger structure
+TERNARY(slice)	// struct, begin, end: extract elements as a smaller struct
+
+// array-specific operations
+UNARY(empty) 	// array -> bool: true if array contains no elements
 UNARY(peek)		// array -> value: value of first array element
 UNARY(next)		// array -> array: array containing all but the first element
-BINARY(call)	// block, val: transfer control and supply an argument value
-BINARY(bind)	// block, val -> closure: bind arg to block, creating a closure
-BINARY(diff)	// int, int -> int: arithmetic difference
-BINARY(xorl)	// bool, bool -> bool: true if exactly one input is true
-				// int, int -> int: exclusive or on each bit in the word
 BINARY(item)	// array, int -> value: get N'th array element value
 BINARY(head)	// array, int -> array: first N elements
 BINARY(skip)	// array, int -> array: all but the first N elements
 BINARY(tail)	// array, int -> array: last N elements
 BINARY(drop)	// array, int -> array: all but the last N elements
-TERNARY(bcc)	// bool, closure, closure: if the condition is true, jump to
-				// the first closure, otherwise jump to the second one.
-TERNARY(sel)	// bool, value, value: return the first value if the condition
-				// is true, otherwise return the second value.
-TERNARY(loop)	// bool, value, block: if the condition is true, reinvoke the
-				// current block with a new argument value; otherwise, call
-				// the target function with that argument value.
-VARIADIC(sum)	// int* -> int: add up all the values
-VARIADIC(andl)	// bool* -> bool: true if all values are true
-				// int* -> int: each bit set if it is set in all inputs
-VARIADIC(orl)	// bool* -> bool: true if any value is true
-				// int* -> int: each bit set if it is set in any input
-VARIADIC(cpeq)	// int* -> bool: true if all values are equal
-VARIADIC(cpge)	// int* -> bool: true if values are equal or ascending
-VARIADIC(cpgt)	// int* -> bool: true if values are non-equal and ascending
-VARIADIC(tuple)	// value* -> tuple: create a structure from these values
-VARIADIC(array)	// value* -> array: create an array from equal-typed values
-VARIADIC(cat)	// array* -> array: join the contents of these arrays
 
 #undef UNARY
 #undef BINARY
